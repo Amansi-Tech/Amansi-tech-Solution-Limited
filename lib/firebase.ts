015d042317807c0fa16b63be20d22a59a7b6ef08
Amansi-codes <p.star.chinedu@gmail.com>
2025-07-25 14:52:58 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,11 +3,6 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// lib/firebase.ts
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCkSGqjWWojfLZD1A5Cl5ABlvhb6ZZTCCk",
   authDomain: "amansi-tech.firebaseapp.com", // ✅ Replace with actual
@@ -22,10 +17,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
 
 export { db, app, auth };
 
