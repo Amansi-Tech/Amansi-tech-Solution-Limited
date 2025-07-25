ec446a1ca662d319cd903bf543046248a5f747d1
Amansi-codes <p.star.chinedu@gmail.com>
2025-07-25 14:48:54 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,10 +11,10 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCkSGqjWWojfLZD1A5Cl5ABlvhb6ZZTCCk",
   authDomain: "amansi-tech.firebaseapp.com", // ✅ Replace with actual
-  projectId: "amansi-tech",
+  projectId: "project-39eda",
   storageBucket: "amansi-tech.appspot.com",
-  messagingSenderId: "XXXXXXXXXXX",
-  appId: "1:XXXXXXXXXXXX:web:XXXXXXXXXXXXXX",
+  messagingSenderId: "322454462858",
+  appId: "1:322454462858:web:e480b551893f142d456873",
 };
 
 const app = initializeApp(firebaseConfig);
